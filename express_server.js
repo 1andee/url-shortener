2a33a49492335606e597ce2f587269c066663dff
Ian Duke <ian.c.duke@gmail.com>
2017-06-06 19:09:47 -0400

    Basic functionality added for new URL submission

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,11 +21,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  console.log("urls_new");
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
+  console.log("/urls/:id");
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.... is there anybody out there?`);
 });
+
+// function generateRandomString() {
+// }
