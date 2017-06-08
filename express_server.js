fc6b8e313b56d6c43dffe2f1c3ff7f66b8f3f912
Ian Duke <ian.c.duke@gmail.com>
2017-06-08 15:55:16 -0400

    Added and tested registration feature. Works.

--- express_server.js
+++ express_server.js
@@ -1,38 +1,65 @@
-var express = require("express");
-var app = express();
-var randomizer = require("./randomizer");
-var protocolChecker = require("./protocolChecker");
-var cookieParser = require('cookie-parser');
+const express = require("express");
+const app = express();
+const randomizer = require("./randomizer");
+const protocolChecker = require("./protocolChecker");
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-var PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 app.get("/", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    users, user_id: req.cookies["user_id"]
   };
   res.render("index", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  let templateVars = {
+    users, user_id: req.cookies["user_id"]
+  };
+  res.render("register", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = {
+    users, user_id: req.cookies["user_id"]
+  };
+  res.render("login", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    users, user_id: req.cookies["user_id"]
   };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    users, user_id: req.cookies["user_id"]
   };
   res.render("urls_new", templateVars);
 });
@@ -41,7 +68,7 @@ app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    users, user_id: req.cookies["user_id"]
   };
   res.render("urls_show", templateVars);
 });
@@ -51,9 +78,38 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
+app.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  // Conditional checks for email
+  if (email.length <= 5) {
+      res.status(400).send("Error 400: Please provide a valid email.");
+  };
+  for (key in users) {
+    if (users[key].email === email) {
+      res.status(400).send("Error 400: This email is already associated with an account in our system. Please register with a different one.");
+    };
+  };
+  // Conditional checks for password
+  if (!password) {
+    res.status(400).send("Error 400: Please provide a password.");
+  };
+  let user_id = randomizer();
+  res.cookie('user_id', user_id);
+  users[user_id] = {id: user_id, email: email, password: password};
   res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  for (key in users) {
+    if (users[key].email === req.body.email) {
+      if (users[key].password === req.body.password) {
+        res.cookie('user_id', users[key].id);
+        res.redirect("/urls");
+      }
+    }
+  }
+  return res.status(403).send("Please check your username and/or password.");
 });
 
 app.post("/urls", (req, res) => {
@@ -73,7 +129,7 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.body.username);
+  res.clearCookie('user_id', req.body.user_id);
   res.redirect("/");
 });
 
